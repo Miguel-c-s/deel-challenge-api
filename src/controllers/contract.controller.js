d29ac5cb217957cd14c37cae01015b1767a5a3c8
Miguel <miguel.saraiva@tecnico.ulisboa.pt>
2022-07-02 19:23:59 +0100

    fix: get contract EP

--- src/controllers/contract.controller.js
+++ src/controllers/contract.controller.js
@@ -1,14 +1,18 @@
+const { Op } = require("sequelize");
 
 module.exports = {
     getContract: async (req, res) => {
-        const {Contract} = req.app.get('models');
-        const {id} = req.params;
-        const contract = await Contract.findOne({where: {id}});
-        if(!contract) return res.status(404).end();
+        const { Contract } = req.app.get('models');
+        const { id } = req.params;
+        const { id: profileId } = req.profile
+        const contract = await Contract.findOne({ where: { id, [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }], } });
+    
+        if (!contract) return res.status(404).send('Contract not found');
+    
         res.json(contract);
     },
 
     getNonTerminatedContracts: async (req, res) => {
-        
+
     },
 }
