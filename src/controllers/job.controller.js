a042ee0970f3aa5efd12254d77d53a00d0230407
Miguel <miguel.saraiva@tecnico.ulisboa.pt>
2022-07-02 20:09:17 +0100

    add: get unpaid jobs EP

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -2,6 +2,29 @@ const { Op } = require("sequelize");
 
 module.exports = {
     getUnpaidJobs: async (req, res, next) => {
+        try {
+            const { Contract, Job } = req.app.get('models');
+            const { id: profileId } = req.profile;
+
+            const jobs = await Job.findAll({
+                where: { paid: { [Op.or]: { [Op.eq]: false, [Op.eq]: null } } },
+                include: [{
+                    model: Contract,
+                    attributes: [],
+                    where: {
+                        [Op.or]: [
+                            { ContractorId: profileId },
+                            { ClientId: profileId },
+                        ],
+                        status: 'in_progress',
+                    }
+                }],
+            });
+
+            res.json(jobs);
+        } catch (e) {
+            next(e);
+        }
 
     },
 
