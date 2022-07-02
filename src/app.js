8468b8c09ecd676e154dd68fb26d9de9612756fe
Miguel <miguel.saraiva@tecnico.ulisboa.pt>
2022-07-02 19:43:16 +0100

    add: error handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model');
-const {getProfile} = require('./middleware/getProfile');
+const { sequelize } = require('./model');
+const { getProfile } = require('./middleware/getProfile');
 
 const routes = require('./routes');
 
@@ -12,5 +12,7 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 app.use('', routes);
+
+app.use((err, req, res, next) => { res.status(500).send('Something broke! Sorry :(') });
 
 module.exports = app;
--- src/controllers/contract.controller.js
+++ src/controllers/contract.controller.js
@@ -1,15 +1,25 @@
 const { Op } = require("sequelize");
 
 module.exports = {
-    getContract: async (req, res) => {
+    getContract: async (req, res, next) => {
         const { Contract } = req.app.get('models');
         const { id } = req.params;
-        const { id: profileId } = req.profile
-        const contract = await Contract.findOne({ where: { id, [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }], } });
-    
-        if (!contract) return res.status(404).send('Contract not found');
-    
-        res.json(contract);
+        const { id: profileId } = req.profile;
+        try {
+            const contract = await Contract.findOne({ 
+                where: { 
+                    id, 
+                    [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }], 
+                } });
+        
+            if (!contract) return res.status(404).send('Contract not found');
+        
+            res.json(contract);
+
+        } catch (e) {
+            next(e);
+        }
+
     },
 
     getNonTerminatedContracts: async (req, res) => {
